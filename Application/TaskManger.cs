5fbd143a50c4116f143a928daeba99bbe1be1e29
Charles-04 <charlesmir04@gmail.com>
2022-12-15 09:32:18 +0100

    Add project files.

---
+++ Application/TaskManger.cs
@@ -0,0 +1,60 @@
+﻿using System.Diagnostics;
+
+namespace TaskManager.Application
+{
+    internal partial class TaskManger
+    {
+       
+        public static void ListAllRunningTasks()
+        {
+         
+          var runningTasks = Process.GetProcesses().OrderBy((x) => (x.ProcessName));    
+
+
+            foreach (var task in runningTasks)
+            {
+                Console.WriteLine($" Task ID: {task.Id}\t TaskName: {task.ProcessName}");
+            }
+        }
+        public static void KillATask()
+        {
+            try
+            {
+                Console.WriteLine("Enter Task name");
+                var name = Console.ReadLine();
+
+                foreach (var item in Process.GetProcessesByName(name))
+                {
+                    item.Kill(true);
+                }
+
+            }
+            catch (InvalidOperationException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+        public static void StartANewTask()
+        {
+
+            Process? process = null;
+
+        StartTask: try
+            {
+                Console.WriteLine("Enter Task name");
+                var name = Console.ReadLine();
+                process = Process.Start(@$"{name}");
+            }
+            catch (InvalidOperationException ex)
+            {
+                Console.WriteLine(ex.Message);
+                goto StartTask;
+            }
+        }
+        public static void StartAThread()
+        {
+            Thread newThread = new();
+        }
+
+    }
+}
---
+++ Utility/Util.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManager.Utility
+{
+    
+    internal static class Util
+    {
+        private static StringBuilder stringBuilder = new();
+        public static void MakeMenu()
+        {
+            stringBuilder.AppendLine($"Task Manager\n \n \n");
+            stringBuilder.AppendLine($"Options");
+            stringBuilder.AppendLine($"1 Start a Task");
+            stringBuilder.AppendLine($"2 Stop a Task");
+            stringBuilder.AppendLine($"3 Get all current Tasks");
+            stringBuilder.AppendLine($"4 Start custom process");
+            stringBuilder.AppendLine($"6 Create a background thread");
+            stringBuilder.AppendLine($"7 Check thread status (Live or Background)");
+
+        }
+        
+        public static void DisplayMenu()
+        {
+            Console.WriteLine(stringBuilder);
+        }
+        
+    }
+}
